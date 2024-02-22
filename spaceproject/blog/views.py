cd754124664ae77c9ce3734b952c32463f538d71
Blackdrive9 <davidegrasso966@gmail.com>
2024-02-22 12:36:21 +0100

    first commit

--- spaceproject/blog/views.py
+++ spaceproject/blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Post
 
 
@@ -11,3 +11,9 @@ def post_list(request):
     context = {'posts': posts}
 
     return render(request, 'blog/post_list.html', context)
+
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+
+    return render(request, 'blog/post_detail.html', {'post': post})
